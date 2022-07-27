ae8e0c22e638c170fa4a4e25b5f5b53e2b4cd570
fgfrutos <fgfrutos@gmail.com>
2022-07-27 19:45:58 +0200

    Change calculator size and improved input interaction

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // UI
 const screen = document.querySelector('#screen');
 const text = document.querySelector('#screen > p');
-text.style.fontSize = '2.5rem';
+text.style.fontSize = '5rem';
 const clearBtn = document.querySelector('.clear');
 const signBtn = document.querySelector('.sign');
 const numBtn = Array.from(document.querySelectorAll('div[class=button]'));
@@ -34,9 +34,9 @@ const calculator = {
  }
 
 // Functions
-const changeSize = (element) => {
+const changeSize = (element, change) => {
     Size = element.style.fontSize.replace('rem', '')
-    return `${parseFloat(Size)*.89}rem`
+    return (change === 1)? `${parseFloat(Size)*1.12}rem`: `${parseFloat(Size)*.88}rem`;
 }
 
 // Function that formate numbers
@@ -52,8 +52,7 @@ const formatting = (x) => {
 numBtn.forEach(btn => {
     btn.addEventListener('click', () => {
         let tmpText = text.textContent.replace(",", "");
-        const condition = (tmpText.match("\\.") === null)?tmpText.length < 10:tmpText.length < 11;
-        if (condition) {
+        if (tmpText.length < 10) {
             if (tmpText === '0' || tmpText === '-0') {
                 tmpText = tmpText.replace('0', '');
                 clearBtn.textContent = 'C';
@@ -62,8 +61,8 @@ numBtn.forEach(btn => {
             tmpText += btn.textContent;
             text.textContent = formatting(tmpText);
 
-            if (text.clientWidth > screen.clientWidth-2) {
-                text.style.fontSize = changeSize(text)
+            if (text.clientWidth > screen.clientWidth-10) {
+                text.style.fontSize = changeSize(text, 0)
             }
         }
     });
@@ -71,24 +70,29 @@ numBtn.forEach(btn => {
 
 commaBtn.addEventListener('click', () => {
     // Needs to use double backslash to scape dot special character (find all)
-    if (text.textContent.match("\\.") === null) {
+    if (text.textContent.match("\\.") === null && text.textContent.replaceAll(",", "").length !==9) {
         text.textContent += "."
     }
 });
 
 signBtn.addEventListener('click', () =>{
     if (text.textContent[0] === '-') {
-        text.textContent.replace("-", "");
+        text.textContent = text.textContent.replace("-", "");
+        const condition = text.textContent.replace(/[.,]/g, "").length === 9 && text.clientWidth > screen.clientWidth-10
+        if (condition) {
+            text.style.fontSize = changeSize(text, 1);
+        }
     } else {
         text.textContent = '-'+text.textContent;
     }
-    if (text.clientWidth > screen.clientWidth-2) {
+    if (text.clientWidth > screen.clientWidth-10) {
         text.style.fontSize = changeSize(text)
     }
 });
 
 
 clearBtn.addEventListener('click', () =>{
+    text.style.fontSize = '5rem';
     clearBtn.textContent = 'AC';
     text.textContent = '0';
 });
